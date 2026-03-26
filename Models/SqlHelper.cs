736a03063fb432f311d9663a4dccdde120dde565
aslisudesahin <aslisahin357@gmail.com>
2026-03-26 21:40:38 +0300

    Add files via upload

---
+++ Models/SqlHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms; 
+
+namespace FlightReservationSystem.DataAccess 
+{
+    public class SqlHelper
+    {
+        private readonly string connectionString = @"Server=(localdb)\MSSQLLocalDB; Database=UcakSistemDB; Integrated Security=True;";
+
+       
+        public object ExecuteScalar(string query, params SqlParameter[] parameters)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    try
+                    {
+                        connection.Open();
+                        return command.ExecuteScalar();
+                    }
+                    catch (SqlException ex)
+                    {
+                        
+                        MessageBox.Show("Veritabanı bağlantısı veya sorgu hatası oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return null;
+                    }
+                }
+            }
+        }
+
+       
+
+       
+        public int ExecuteNonQuery(string query, params SqlParameter[] parameters)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    try
+                    {
+                        connection.Open();
+                        return command.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Veri Ekleme/Güncelleme Hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return -1;
+                    }
+                }
+            }
+        }
+
+
+        public DataTable GetData(string query, params SqlParameter[] parameters)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    try
+                    {
+                        connection.Open();
+                        
+                        SqlDataAdapter adapter = new SqlDataAdapter(command);
+                        adapter.Fill(dataTable);
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Veri Çekme Hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        
+                        return null;
+                    }
+                }
+            }
+            return dataTable;
+        }
+    }
+}
