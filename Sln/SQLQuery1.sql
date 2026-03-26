1e351862fb970ba28ac2947cf0a00a30dae5d475
aslisudesahin <aslisahin357@gmail.com>
2026-03-26 21:39:45 +0300

    Add files via upload

---
+++ Sln/SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Musteriler] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [KullaniciAdi] NVARCHAR (50) NULL,
+    [Sifre]        NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
